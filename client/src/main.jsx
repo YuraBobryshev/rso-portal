f3883b0d12358f316dc30fde78dbf4912426b4e3
YuraBobryshev <urabobrenok2005@gmail.com>
2026-05-17 16:05:33 +0300

    fix: add global axios interceptor for dynamic production url

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import axios from 'axios';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+axios.interceptors.request.use((config) => {
+  if (config.url && config.url.includes('http://localhost:5000')) {
+    config.url = config.url.replace(
+      'http://localhost:5000', 
+      `http://${window.location.hostname}:5000`
+    );
+  }
+  return config;
+});

01f8b9d2832111fc6662ab532ff67adfdf267c36
YuraBobryshev <urabobrenok2005@gmail.com>
2026-05-29 23:04:43 +0300

    Темная тема1 фикс

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { GoogleOAuthProvider } from '@react-oauth/google'; // ДОБАВИЛИ ИМПОРТ
-import { ThemeProvider } from './contexts/ThemeContext.jsx' // <--- Импорт
+import { GoogleOAuthProvider } from '@react-oauth/google'; 
+import { ThemeProvider } from './contexts/ThemeContext.jsx'; // <--- Наш импорт
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* ОБЕРНУЛИ APP В ПРОВАЙДЕР С ТВОИМ CLIENT_ID */}
     <GoogleOAuthProvider clientId="434402129133-t580icfrhgespoptbj65kevs87808a3f.apps.googleusercontent.com">
-      <App />
+      {/* ВОТ ОН! Оборачиваем App в провайдер темы */}
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
